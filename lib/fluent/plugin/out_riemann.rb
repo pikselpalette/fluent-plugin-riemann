ed11c59f0b755ba0225132d2d68eaa2db6573a20
Stephen Gran <stephen.gran@piksel.com>
2014-11-14 14:36:42 +0000

    allow for dynamic metrics
    Signed-off-by: Stephen Gran <stephen.gran@piksel.com>

--- lib/fluent/plugin/out_riemann.rb
+++ lib/fluent/plugin/out_riemann.rb
@@ -3,13 +3,11 @@ require 'riemann/client'
 class Fluent::RiemannOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('riemann', self)
 
-  config_param :host, :string, :default => '127.0.0.1'
-  config_param :post, :integer, :default => 5555
-  config_param :timeout, :integer, :default => 5
-  config_param :protocol, :string, :default => 'tcp'
-  config_param :service, :string, :default => nil
-  config_param :types, :string, :default => 'metric:float'
-  config_param :fields, :string, :default => 'message:description,level:state,metric'
+  config_param :host,     :string,  :default => '127.0.0.1'
+  config_param :port,     :integer, :default => 5555
+  config_param :timeout,  :integer, :default => 5
+  config_param :protocol, :string,  :default => 'tcp'
+  config_param :fields,   :hash,    :default => {}
 
   def initialize
     super
@@ -18,16 +16,6 @@ class Fluent::RiemannOutput < Fluent::BufferedOutput
   def configure(c)
     super
 
-    @_types = parse_map(@types) do |k, t|
-      case t
-      when "string" then "to_s"
-      when "integer" then "to_i"
-      when "float" then "to_f"
-      else t
-      end
-    end
-
-    @_fields = parse_map(@fields) { |k, f| (f || k).to_sym }
   end
 
   def start
@@ -47,31 +35,32 @@ class Fluent::RiemannOutput < Fluent::BufferedOutput
     [tag, time, record].to_msgpack
   end
 
+  def remap(data)
+    if data.is_a? String
+      if data =~ /^\d+\.\d+$/
+        data = data.to_f
+      elsif data =~ /^\d+$/
+        data = data.to_i
+      else
+        data = nil
+      end
+    end
+    data
+  end
+
   def write(chunk)
     chunk.msgpack_each do |tag, time, record|
       event = {
-        :time => time,
-        :service => @service,
-        :tags => tag.split('.')
+        :time    => time,
+      }
+      event.merge!(@fields)
+      record.each { |k, v|
+        next unless v = remap(v)
+        event[:service] = k.gsub(/\./, ' ')
+        event[:metric] = v.to_f
+        client << event
       }
 
-      @_fields.each { |k,v| event[v] = record[k] }
-      @_types.each { |k,t| event[k] = event[k].send(t) }
-
-      client << event
     end
-  end
-
-  private
-
-  def parse_map(map, &block)
-    Hash[map.split(',').map do |m|
-      k, v = m.split(':').map(&:strip)
-      if block_given?
-        [k, yield(k, v)]
-      else
-        [k, v]
-      end
-    end]
   end
 end
