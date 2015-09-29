e6812ce1f0c6cf3d137da61863a2a28b36ac6421
Craig Genner <craig.genner@piksel.com>
2015-09-29 13:03:21 +0100

    better name for variable

--- lib/fluent/plugin/out_riemann.rb
+++ lib/fluent/plugin/out_riemann.rb
@@ -51,12 +51,12 @@ class Fluent::RiemannOutput < Fluent::BufferedOutput
   end
 
   def write(chunk)
-    expiredtime = Time.now.to_i - @ttl.to_i
+    expiretime = Time.now.to_i - @ttl.to_i
     chunk.msgpack_each do |tag, time, record|
       record.each { |k, v|
         next unless v = remap(v)
 
-	if time.to_i < expiredtime
+	if time.to_i < expiretime
           log.warn "Dropping event, past the ttl."
           next
         end
